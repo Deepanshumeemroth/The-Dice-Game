8b7ada213490526df51379ef7af3485df88a4239
deepanshumeemroth <deepanshu6479@gmail.com>
2021-01-22 04:53:35 +0530

    New Commit

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 // document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
 // document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
 
+
 if(randomNumber1 > randomNumber2){
   document.querySelector("h1").innerHTML = "Player 1 Wins!🚩";
 }
